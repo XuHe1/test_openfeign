33107ca2cf471bfdceac7fae51445ff2d2c4545e
DESKTOP-SSC4UP3\huilianyun <1090624761@qq.com>
2023-04-21 09:54:49 +0800

    add  loadbalancer

--- svc_provider/src/main/java/top/lovezhangli/provider/ProviderApplication.java
+++ svc_provider/src/main/java/top/lovezhangli/provider/ProviderApplication.java
@@ -1,8 +1,11 @@
 package top.lovezhangli.provider;
 
+import com.alibaba.cloud.nacos.ribbon.NacosRule;
+import com.netflix.loadbalancer.IRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,11 @@ public class ProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
+    }
+
+    @Bean
+    public IRule iRule(){
+        return  new NacosRule();
     }
 
 }
